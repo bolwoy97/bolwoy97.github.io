684ce73338d7cbd7c61d97434da98f30c8fb4fbd
Leonid Shadrin <bolwoy97@gmail.com>
2023-03-11 12:09:53 +0200

    competencies fetch

--- src/components/flows/resume/Competencies.tsx
+++ src/components/flows/resume/Competencies.tsx
@@ -1,56 +1,32 @@
+import { useEffect, useState } from "react";
+
 export default function Competencies(): JSX.Element {
+  const [competencies, setCompetencies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/main_page/competencies")
+      .then((response) => response.json())
+      .then(({ data }) => {
+        setCompetencies(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
   return (
     <>
       <h2>Competencies</h2>
       <ul>
-        <li>
-          <p>JavaScript</p>
-          <div className="progress-bar">
-            <div className="progress" style={{ width: "80%" }}></div>
-          </div>
-        </li>
-        <li>
-          <p>TypeScript</p>
-          <div className="progress-bar">
-            <div className="progress" style={{ width: "75%" }}></div>
-          </div>
-        </li>
-        <li>
-          <p>React.js</p>
-          <div className="progress-bar">
-            <div className="progress" style={{ width: "60%" }}></div>
-          </div>
-        </li>
-        <li>
-          <p>Node.js</p>
-          <div className="progress-bar">
-            <div className="progress" style={{ width: "80%" }}></div>
-          </div>
-        </li>
-        <li>
-          <p>HTML</p>
-          <div className="progress-bar">
-            <div className="progress" style={{ width: "60%" }}></div>
-          </div>
-        </li>
-        <li>
-          <p>CSS</p>
-          <div className="progress-bar">
-            <div className="progress" style={{ width: "50%" }}></div>
-          </div>
-        </li>
-        <li>
-          <p>MongoDB</p>
-          <div className="progress-bar">
-            <div className="progress" style={{ width: "75%" }}></div>
-          </div>
-        </li>
-        <li>
-          <p>MySQL</p>
-          <div className="progress-bar">
-            <div className="progress" style={{ width: "75%" }}></div>
-          </div>
-        </li>
+        {competencies.map(({name, value}) => {
+          return (
+            <li>
+              <p>{name}</p>
+              <div className="progress-bar">
+                <div className="progress" style={{ width: `${value}%` }}></div>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
